fa29be575dc9e233ab7ced61e7f7e1c183cdcccd
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2026-01-14 20:22:08 +0100

    new : search bar in home page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+use App\Form\PokemonSearchType;
 use App\Service\Pokeapi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(Pokeapi $pokeapi) {
+    public function index(Pokeapi $pokeapi, Request $request) {
         $pokemons = $pokeapi->pokemonGetAllv2(9);
-      
+        $form = $this->createForm(PokemonSearchType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $name = $data['search'];
+            return $this->redirectToRoute('pokemon', ['name' => $name]);
+        }
         return $this->render('main/index.html.twig', [
-            'pokemons' => $pokemons
+            'pokemons' => $pokemons,
+            'form' => $form
         ]);
     }
 
