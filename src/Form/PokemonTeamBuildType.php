9a118548c5cf3c2eeb711543f126307bed5ac6ec
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2026-02-08 22:52:25 +0100

    new front css

--- src/Form/PokemonTeamBuildType.php
+++ src/Form/PokemonTeamBuildType.php
@@ -13,12 +13,12 @@ class PokemonTeamBuildType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name1', TextType::class, ['required' => false, 'label' => 'Name of pokemon : '])
-            ->add('name2', TextType::class, ['required' => false, 'label' => 'Name of pokemon : '])
-            ->add('name3', TextType::class, ['required' => false, 'label' => 'Name of pokemon : '])
-            ->add('name4', TextType::class, ['required' => false, 'label' => 'Name of pokemon : '])
-            ->add('name5', TextType::class, ['required' => false, 'label' => 'Name of pokemon : '])
-            ->add('name6', TextType::class, ['required' => false, 'label' => 'Name of pokemon : '])
+            ->add('name1', TextType::class, ['required' => false, 'label' => false, 'attr' => ['placeholder' => '1er pokémon']])
+            ->add('name2', TextType::class, ['required' => false, 'label' => false, 'attr' => ['placeholder' => '2ème pokémon']])
+            ->add('name3', TextType::class, ['required' => false, 'label' => false, 'attr' => ['placeholder' => '3ème pokémon']])
+            ->add('name4', TextType::class, ['required' => false, 'label' => false, 'attr' => ['placeholder' => '4ème pokémon']])
+            ->add('name5', TextType::class, ['required' => false, 'label' => false, 'attr' => ['placeholder' => '5ème pokémon']])
+            ->add('name6', TextType::class, ['required' => false, 'label' => false, 'attr' => ['placeholder' => '6ème pokémon']])
             // ->add('name1', ChoiceType::class, [
             //     'choices' => ['bulbizarre' => 1, 'salameche' => 4, 'carapuce' => 7],
             //     'required' =>  false
