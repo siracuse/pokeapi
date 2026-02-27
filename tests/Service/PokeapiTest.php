8d82a148de7ea49c147fcb415c939d17690968b0
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2026-02-27 16:59:46 +0100

    new = test unitaire

---
+++ tests/Service/PokeapiTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+use App\Service\Pokeapi;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+class PokeapiTest extends TestCase
+{
+    
+    private Pokeapi $service;
+
+    protected function setUp(): void
+    {
+        $mockClient = $this->createMock(HttpClientInterface::class);
+        $mockCache = $this->createMock(CacheInterface::class);
+
+        $this->service = new Pokeapi(
+            $mockClient,
+            $mockCache,
+            3600
+        );
+    }
+
+
+    public function testNameSearchToId()
+    {
+        $result = $this->service->nameSearchToId('pikachu');
+
+        $this->assertEquals(25, $result);
+    }
+
+}
